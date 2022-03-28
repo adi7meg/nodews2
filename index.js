721943260c904bfd92372c48f59a2d05e32b75f5
Aditya sharma <adityasharma@Adityas-MacBook-Air.local>
2022-03-29 02:45:01 +0530

    add a controller and a router for home

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8000;
 
 
+
+app.use('/', require('./routes'));
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running server: ${err}`);
