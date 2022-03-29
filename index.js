3b47d9de02a321f5f4784ecf09afdd2d8297c640
Aditya sharma <adityasharma@Adityas-MacBook-Air.local>
2022-03-29 16:15:21 +0530

    installing ejs and setup view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 
 app.use('/', require('./routes'));
 
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running server: ${err}`);
