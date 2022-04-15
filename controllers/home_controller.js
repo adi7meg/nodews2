8d3d2e414e9bd9818706099cdd792d71b4fd2bdb
Aditya sharma <adityasharma@Adityas-MacBook-Air.local>
2022-04-15 14:22:34 +0530

    display posts and redated user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,24 @@
-module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',255);
-    return res.render('home',{
-        title:"Home"
-    });
-};
+const Post = require('../models/post');
 
+module.exports.home = function(req,res){
+    // console.log(req.cookies);
+    // res.cookie('user_id',255);
+
+    // Post.find({},function(err,posts){
+
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts: posts 
+    // });
+    // });
+
+//populate the user of each post
+
+    Post.find({}).populate('user').exec(function(err,posts){
+            return res.render('home',{
+            title:"Codeial | Home",
+            posts: posts 
+            });
+    });
+
+};
