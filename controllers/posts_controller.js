2846a445bbea6f55123f3543afe9ea31dbc50d6c
Aditya sharma <adityasharma@Adityas-MacBook-Air.local>
2022-04-26 01:28:31 +0530

    Creating Flash Messages

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,28 +3,44 @@ const Post = require('../models/post');
 const Comment = require('../models/comment');
 
 
-module.exports.create = function(req,res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    },function(err,post){
-        if(err){console.log('error in creating the post'); return;}
+module.exports.create = async function(req,res){
+    
+    try{
+
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
         return res.redirect('back');
-    });
+
+    }catch(err){
+        console.log("Error",err);
+        return;
+    }
+  
 }
 
 
-module.exports.destroy = function(req,res){
+module.exports.destroy = async function(req,res){
+    
+    try{
 
-    Post.findById(req.params.id,function(err,post){
-//  .id means converting the object id into string
-        if(post.user == req.user.id){
-            post.remove();
+        let post = await Post.findById(req.params.id)
+        //  .id means converting the object id into string
+            
+            if(post.user == req.user.id){
+                post.remove();
+        
+               await Comment.deleteMany({post: req.params.id});
+                    return res.redirect('back');
+            }else{ 
+                return res.redirect('back');
+            }
 
-            Comment.deleteMany({post: req.params.id},function(err){
+    }catch(err){
+        console.log("Error",err);
+        return;
+    }
 
-                return res.redirect('/');
-            });
-        }else{ return res.redirect('/');}
-    });
 };
+
