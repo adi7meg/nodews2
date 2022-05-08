d063142e2267a8ee864e54be79ce5a1788cf6bff
Aditya sharma <adityasharma@Adityas-MacBook-Air.local>
2022-05-09 03:47:07 +0530

    setting up NodeMailer

---
+++ config/nodemailer.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+
+
+
+//This part sends the Emails
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'alchemy.cn18',
+        pass: 'codingninjas'
+    }
+});
+
+
+let renderTemplate = (data,relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname,'../views/mailers',relativePath),
+        data,
+        function(err,template){
+            if(err){comsole.log('error in rendering template'); return}
+
+            mailHTML = template;
+        }
+    )
+    return mailHTML;
+};
+
+
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+};
